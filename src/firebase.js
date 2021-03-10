4a9acb0d5ef1d4dab822a9a430924c897beff03c
altaf53 <aaltafali@gmail.com>
2021-03-11 02:50:05 +0530

    first

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCqHMFLcU8Gc5-ro3Z48s2doxYpU2SxZf0",
+    authDomain: "netflix-clone-28523.firebaseapp.com",
+    projectId: "netflix-clone-28523",
+    storageBucket: "netflix-clone-28523.appspot.com",
+    messagingSenderId: "523986784234",
+    appId: "1:523986784234:web:d7cb38ecce3764479bec93"
+  };
